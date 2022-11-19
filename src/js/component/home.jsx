148b05f123c150fee5876a442a25a1c30e006cbe
pzagmel <pedro.zagalm@gmail.com>
2022-11-19 15:08:53 +0000

    fist commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [currentValue, setValue] = useState("");
+
+  const data=["Make the bed", "Wash my hands", "Eat", "Walk the Dog"];
+  
+
+  return (
+    <div className="text-center">
+      <h1>My Tasks</h1>
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={currentValue}
+        placeholder="What needs to be done?"
+      />
+    </div>
+  );
 };
 
 export default Home;
