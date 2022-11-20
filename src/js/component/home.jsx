6ebf953515db1a27b26a9845ecfb6e2b74dfc664
pzagmel <pedro.zagalm@gmail.com>
2022-11-20 18:40:46 +0000

    commit2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,20 +5,27 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-  const [currentValue, setValue] = useState("");
-
-  const data=["Make the bed", "Wash my hands", "Eat", "Walk the Dog"];
-  
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
   return (
-    <div className="text-center">
+    <div className="container">
       <h1>My Tasks</h1>
-      <input
-        type="text"
-        onChange={(e) => setValue(e.target.value)}
-        value={currentValue}
-        placeholder="What needs to be done?"
-      />
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+              setTodos(todos.concat(inputValue));
+              }
+            }            
+          }
+              placeholder="What needs to be done?"></input>
+        </li>
+      </ul>
     </div>
   );
 };
